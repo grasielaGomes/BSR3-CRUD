5440842faaa6498552ed0916dd5bac9507705296
Grasiela Gomes <produtoradama@gmail.com>
2021-05-18 15:32:50 -0300

    Clients by id

--- backend/src/main/java/com/grasielagomes/clients/services/ClientService.java
+++ backend/src/main/java/com/grasielagomes/clients/services/ClientService.java
@@ -2,6 +2,7 @@ package com.grasielagomes.clients.services;
 
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.grasielagomes.clients.dto.ClientDTO;
 import com.grasielagomes.clients.entities.Client;
 import com.grasielagomes.clients.repositories.ClientRepository;
+import com.grasielagomes.clients.services.exceptions.ResourceNotFoundedException;
 
 @Service
 public class ClientService {
@@ -23,13 +25,35 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());		
+		return list.stream().map(c -> new ClientDTO(c)).collect(Collectors.toList());		
 	}
 	
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
-		return list.map(x -> new ClientDTO(x));		
+		return list.map(c -> new ClientDTO(c));		
 	}
 
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundedException("Entity not found"));
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	private void copyDtoEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+	}
 }
